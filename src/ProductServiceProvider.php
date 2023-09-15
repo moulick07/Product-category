373ab767fef1ba4e69e7bed8051d2c2266c46d0f
moulick.r <moulick.r@webcodegenie.com>
2023-09-15 16:07:30 +0530

    my first commit

---
+++ src/ProductServiceProvider.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Product\Category;
+
+use Illuminate\Support\ServiceProvider;
+
+class ProductServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        //
+    }
+}
---
+++ src/test.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Product\Category;
+
+class Test
+{
+    public function greet(String $sName)
+    {
+        return 'Hi ' . $sName . '! How are you doing today?';
+    }
+}
