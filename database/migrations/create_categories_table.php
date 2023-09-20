1d0da9daeac82cb371881aa6a858813b51479864
moulick.r <moulick.r@webcodegenie.com>
2023-09-20 11:34:12 +0530

    error fixed

--- database/migrations/create_categories_table.php
+++ database/migrations/create_categories_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('Products', function (Blueprint $table) {
+        Schema::create('Categories', function (Blueprint $table) {
             $table->id();
             $table->string('title');
             $table->string('description');
