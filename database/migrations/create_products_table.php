741217f6cbfeb7d7ec0e7d5281a61065a8d538cd
moulick.r <moulick.r@webcodegenie.com>
2023-09-20 10:55:07 +0530

    changes in migration file

--- database/migrations/create_products_table.php
+++ database/migrations/create_products_table.php
@@ -14,6 +14,8 @@ return new class extends Migration
         Schema::create('Products', function (Blueprint $table) {
             $table->id();
             $table->string('title');
+            $table->string('price');
+            $table->string('slug');
             $table->timestamps();
         });
     }
