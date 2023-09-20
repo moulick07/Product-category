1391a0d40fe42ffd26581c06104506ea2ad9fdc4
moulick.r <moulick.r@webcodegenie.com>
2023-09-20 16:35:34 +0530

    view file testing

--- src/ProductServiceProvider.php
+++ src/ProductServiceProvider.php
@@ -20,5 +20,7 @@ class ProductServiceProvider extends ServiceProvider
     public function boot(): void
     {
         $this->loadMigrationsFrom(__DIR__.'/../database/migrations/**');
+        $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
+        $this->loadViewsFrom(__DIR__.'/../views', 'inspire');
     }
 }
