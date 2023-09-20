bd31bb12376413422b24dd2395ac028fe0844d87
moulick07 <133027720+moulick07@users.noreply.github.com>
2023-09-20 16:03:52 +0530

    Update ProductServiceProvider.php

--- src/ProductServiceProvider.php
+++ src/ProductServiceProvider.php
@@ -3,6 +3,8 @@
 namespace Product\Category;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Collection;
+use Illuminate\Filesystem\Filesystem;
 
 class ProductServiceProvider extends ServiceProvider
 {
@@ -19,6 +21,21 @@ class ProductServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        $this->loadMigrationsFrom(__DIR__.'/../database/migrations/**/*');
+        
+        $this->publishes([
+            __DIR__.'/../database/migrations/create_products_table.php.stub' => $this->getMigrationFileName('create_products_table.php'),
+        ], 'permission-migrations');
+
+    }
+    protected function getMigrationFileName(string $migrationFileName): string
+    {
+        $timestamp = date('Y_m_d_His');
+
+        $filesystem = $this->app->make(Filesystem::class);
+
+        return Collection::make([$this->app->databasePath().DIRECTORY_SEPARATOR.'migrations'.DIRECTORY_SEPARATOR])
+            ->flatMap(fn ($path) => $filesystem->glob($path.'*_'.$migrationFileName))
+            ->push($this->app->databasePath()."/migrations/{$timestamp}_{$migrationFileName}")
+            ->first();
     }
 }
